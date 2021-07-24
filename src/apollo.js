1fe42867736f75e48049dc54d4dcad8a1e4740a3
yoonsery <yunsel2@gmail.com>
2021-07-25 00:16:31 +0900

    get movies query

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: 'https://movieql2.vercel.app',
+  // uri: 'https://apollo-movie-server.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,3 +1,23 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/client';
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      title
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return 'Loading...🛠';
+  }
+  if (data && data.movies) {
+    return data.movies.map((movie) => <h1 key={movie.id}>{movie.title}</h1>);
+  }
+};
