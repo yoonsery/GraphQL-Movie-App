b468c261053efe752cc6b0bf728cead7de01b4f4
yoonsery <yunsel2@gmail.com>
2021-07-25 13:37:12 +0900

    make movie.js and edit home & detail

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,23 +1,61 @@
 import React from 'react';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/client';
+import Movie from '../components/Movie';
+import styled from 'styled-components';
 
 const GET_MOVIES = gql`
   {
     movies {
       id
-      title
       medium_cover_image
     }
   }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
 export default () => {
   const { loading, data } = useQuery(GET_MOVIES);
-  if (loading) {
-    return 'Loading...🛠';
-  }
-  if (data && data.movies) {
-    return data.movies.map((movie) => <h1 key={movie.id}>{movie.title}</h1>);
-  }
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2021</Title>
+        <Subtitle>I am learning GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...🛠</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map((movie) => <Movie key={movie.id} id={movie.id} />)}
+    </Container>
+  );
 };
